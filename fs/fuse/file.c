05a36612df08dee4a880d4687bf355c6420f3897
Miklos Szeredi <mszeredi@redhat.com>
2017-02-22 20:08:25 +0100

    fuse: release: private_data cannot be NULL
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 9a87ad3da905239413477ac0698734afc5cc30bd)

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -272,16 +272,11 @@ static void fuse_prepare_release(struct fuse_file *ff, int flags, int opcode)
 
 void fuse_release_common(struct file *file, int opcode)
 {
-	struct fuse_file *ff;
-	struct fuse_req *req;
-
-	ff = file->private_data;
-	if (unlikely(!ff))
-		return;
+	struct fuse_file *ff = file->private_data;
+	struct fuse_req *req = ff->reserved_req;
 
 	fuse_passthrough_release(ff);
 
-	req = ff->reserved_req;
 	fuse_prepare_release(ff, file->f_flags, opcode);
 
 	if (ff->flock) {
