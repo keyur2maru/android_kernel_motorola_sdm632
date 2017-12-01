a1618c3224e94a45e4805e362752d47bb9e527e1
Alexei Starovoitov <ast@fb.com>
2017-11-30 21:31:37 -0800

    bpf: don't mark FP reg as uninit
    when verifier hits an internal bug don't mark register R10==FP as uninit,
    since it's read only register and it's not technically correct to let
    verifier run further, since it may assume that R10 has valid auxiliary state.
    
    While developing subsequent patches this issue was discovered,
    though the code eventually changed that aux reg state doesn't have
    pointers any more it is still safer to avoid clearing readonly register.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit d234d462209a819d0ac239c2c854c2b13094af6a)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -584,9 +584,9 @@ static void mark_reg_unknown(struct bpf_verifier_env *env,
 			     struct bpf_reg_state *regs, u32 regno)
 {
 	if (WARN_ON(regno >= MAX_BPF_REG)) {
-		verbose(env, "mark_reg_unknown(env, regs, %u)\n", regno);
-		/* Something bad happened, let's kill all regs */
-		for (regno = 0; regno < MAX_BPF_REG; regno++)
+		verbose(env, "mark_reg_unknown(regs, %u)\n", regno);
+		/* Something bad happened, let's kill all regs except FP */
+		for (regno = 0; regno < BPF_REG_FP; regno++)
 			__mark_reg_not_init(regs + regno);
 		return;
 	}
@@ -603,9 +603,9 @@ static void mark_reg_not_init(struct bpf_verifier_env *env,
 			      struct bpf_reg_state *regs, u32 regno)
 {
 	if (WARN_ON(regno >= MAX_BPF_REG)) {
-		verbose(env, "mark_reg_not_init(env, regs, %u)\n", regno);
-		/* Something bad happened, let's kill all regs */
-		for (regno = 0; regno < MAX_BPF_REG; regno++)
+		verbose(env, "mark_reg_not_init(regs, %u)\n", regno);
+		/* Something bad happened, let's kill all regs except FP */
+		for (regno = 0; regno < BPF_REG_FP; regno++)
 			__mark_reg_not_init(regs + regno);
 		return;
 	}
