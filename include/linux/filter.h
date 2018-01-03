2235a6ef014f23522da7183dd1959c70fb42a248
Jesper Dangaard Brouer <brouer@redhat.com>
2018-01-03 11:25:13 +0100

    xdp: base API for new XDP rx-queue info concept
    This patch only introduce the core data structures and API functions.
    All XDP enabled drivers must use the API before this info can used.
    
    There is a need for XDP to know more about the RX-queue a given XDP
    frames have arrived on.  For both the XDP bpf-prog and kernel side.
    
    Instead of extending xdp_buff each time new info is needed, the patch
    creates a separate read-mostly struct xdp_rxq_info, that contains this
    info.  We stress this data/cache-line is for read-only info.  This is
    NOT for dynamic per packet info, use the data_meta for such use-cases.
    
    The performance advantage is this info can be setup at RX-ring init
    time, instead of updating N-members in xdp_buff.  A possible (driver
    level) micro optimization is that xdp_buff->rxq assignment could be
    done once per XDP/NAPI loop.  The extra pointer deref only happens for
    program needing access to this info (thus, no slowdown to existing
    use-cases).
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 2724b770d3ab4e3ca9eb7f59ad610b628f9b6913)

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -18,6 +18,7 @@
 #include <linux/cryptohash.h>
 #include <linux/kallsyms.h>
 
+#include <net/xdp.h>
 #include <net/sch_generic.h>
 
 #include <asm/cacheflush.h>
@@ -570,6 +571,7 @@ struct xdp_buff {
 	void *data_end;
 	void *data_meta;
 	void *data_hard_start;
+	struct xdp_rxq_info *rxq;
 };
 
 /* Compute the linear packet data range [data, data_end) which
