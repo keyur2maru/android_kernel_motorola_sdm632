2652a27aeb2b2b0e9d0ddd62c47adb7e77b20ca3
John Fastabend <john.fastabend@gmail.com>
2018-01-03 17:57:56 -0800

    bpf: only build sockmap with CONFIG_INET
    The sockmap infrastructure is only aware of TCP sockets at the
    moment. In the future we plan to add UDP. In both cases CONFIG_NET
    should be built-in.
    
    So lets only build sockmap if CONFIG_INET is enabled.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit c3c77f54527684a5c6040acdb1d3243fc2b5c9fe)

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -516,7 +516,7 @@ static inline void __dev_map_flush(struct bpf_map *map)
 }
 #endif /* CONFIG_BPF_SYSCALL */
 
-#if defined(CONFIG_STREAM_PARSER) && defined(CONFIG_BPF_SYSCALL)
+#if defined(CONFIG_STREAM_PARSER) && defined(CONFIG_BPF_SYSCALL) && defined(CONFIG_INET)
 struct sock  *__sock_map_lookup_elem(struct bpf_map *map, u32 key);
 int sock_map_prog(struct bpf_map *map, struct bpf_prog *prog, u32 type);
 #else
