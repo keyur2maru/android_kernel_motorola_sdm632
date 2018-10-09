b21452bd3fc387cd8d837a1fae945771110c9aa1
Prashant Bhole <bhole_prashant_q7@lab.ntt.co.jp>
2018-10-09 10:04:49 +0900

    bpf: error handling when map_lookup_elem isn't supported
    The error value returned by map_lookup_elem doesn't differentiate
    whether lookup was failed because of invalid key or lookup is not
    supported.
    
    Lets add handling for -EOPNOTSUPP return value of map_lookup_elem()
    method of map, with expectation from map's implementation that it
    should return -EOPNOTSUPP if lookup is not supported.
    
    The errno for bpf syscall for BPF_MAP_LOOKUP_ELEM command will be set
    to EOPNOTSUPP if map lookup is not supported.
    
    Signed-off-by: Prashant Bhole <bhole_prashant_q7@lab.ntt.co.jp>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 7caac8c61fdc14815dcf7321124921357e8352f0)

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -710,10 +710,15 @@ static int map_lookup_elem(union bpf_attr *attr)
 			ptr = map->ops->map_lookup_elem_sys_only(map, key);
 		else
 			ptr = map->ops->map_lookup_elem(map, key);
-		if (ptr)
+		if (IS_ERR(ptr)) {
+			err = PTR_ERR(ptr);
+		} else if (!ptr) {
+			err = -ENOENT;
+		} else {
+			err = 0;
 			memcpy(value, ptr, value_size);
+		}
 		rcu_read_unlock();
-		err = ptr ? 0 : -ENOENT;
 	}
 
 	if (err)
