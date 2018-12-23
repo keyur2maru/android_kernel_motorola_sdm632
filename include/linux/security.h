f021978dc91c089d63dfd68c01ad31362306ca3c
Al Viro <viro@zeniv.linux.org.uk>
2018-12-23 16:02:47 -0500

    introduce cloning of fs_context
    new primitive: vfs_dup_fs_context().  Comes with fs_context
    method (->dup()) for copying the filesystem-specific parts
    of fs_context, along with LSM one (->fs_context_dup()) for
    doing the same to LSM parts.
    
    [needs better commit message, and change of Author:, anyway]
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    [keyur: backport to 4.9 - add the ->fs_context_dup LSM hook, but not the
     SELinux/Smack implementations: they duplicate an LSM-owned fc->security blob,
     which only exists after the v5.1 LSM mount-option rework.  On 4.9 the LSM
     options live in fc->secdata (filled by security_sb_copy_data()), so
     vfs_dup_fs_context() copies that page itself.]
    
    (cherry picked from commit 0b52075ee62301dd150c9f2c3ddd0035ed894cde)

--- include/linux/security.h
+++ include/linux/security.h
@@ -227,6 +227,7 @@ int security_bprm_check(struct linux_binprm *bprm);
 void security_bprm_committing_creds(struct linux_binprm *bprm);
 void security_bprm_committed_creds(struct linux_binprm *bprm);
 int security_bprm_secureexec(struct linux_binprm *bprm);
+int security_fs_context_dup(struct fs_context *fc, struct fs_context *src_fc);
 int security_fs_context_parse_param(struct fs_context *fc, struct fs_parameter *param);
 int security_sb_alloc(struct super_block *sb);
 void security_sb_free(struct super_block *sb);
@@ -522,6 +523,12 @@ static inline void security_bprm_committed_creds(struct linux_binprm *bprm)
 static inline int security_bprm_secureexec(struct linux_binprm *bprm)
 {
 	return cap_bprm_secureexec(bprm);
+}
+
+static inline int security_fs_context_dup(struct fs_context *fc,
+					  struct fs_context *src_fc)
+{
+	return 0;
 }
 
 static inline int security_fs_context_parse_param(struct fs_context *fc,
