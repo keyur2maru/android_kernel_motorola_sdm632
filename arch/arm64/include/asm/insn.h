1b5d88ced490846c562681a988444ca012b5450e
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2019-05-24 13:52:20 +0100

    UPSTREAM: arm64: insn: Add BUILD_BUG_ON() for invalid masks
    Detect invalid instruction masks at build time. Some versions of GCC can
    warn about the situation, but not all of them, it seems.
    
    Suggested-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    (cherry picked from commit edbcf50eb8aea5f81ae6d83bb969cb0bc02805a1)
    Signed-off-by: Keyur Maru <kd2maru@gmail.com>

--- arch/arm64/include/asm/insn.h
+++ arch/arm64/include/asm/insn.h
@@ -18,6 +18,7 @@
  */
 #ifndef	__ASM_INSN_H
 #define	__ASM_INSN_H
+#include <linux/build_bug.h>
 #include <linux/types.h>
 
 /* A64 instructions are always 32 bits. */
@@ -292,11 +293,16 @@ enum aarch64_insn_mb_type {
 	AARCH64_INSN_MB_OSHLD,
 };
 
-#define	__AARCH64_INSN_FUNCS(abbr, mask, val)	\
-static __always_inline bool aarch64_insn_is_##abbr(u32 code) \
-{ return (code & (mask)) == (val); } \
-static __always_inline u32 aarch64_insn_get_##abbr##_value(void) \
-{ return (val); }
+#define	__AARCH64_INSN_FUNCS(abbr, mask, val)				\
+static __always_inline bool aarch64_insn_is_##abbr(u32 code)		\
+{									\
+	BUILD_BUG_ON(~(mask) & (val));					\
+	return (code & (mask)) == (val);				\
+}									\
+static __always_inline u32 aarch64_insn_get_##abbr##_value(void)	\
+{									\
+	return (val);							\
+}
 
 __AARCH64_INSN_FUNCS(adr,	0x9F000000, 0x10000000)
 __AARCH64_INSN_FUNCS(adrp,	0x9F000000, 0x90000000)
