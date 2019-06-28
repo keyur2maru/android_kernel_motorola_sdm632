54650646ac3f7ba4efc3a3ce8b951c884de1e0c1
Toke Høiland-Jørgensen <toke@redhat.com>
2019-06-28 11:12:35 +0200

    BACKPORT: devmap: Allow map lookups from eBPF
    We don't currently allow lookups into a devmap from eBPF, because the map
    lookup returns a pointer directly to the dev->ifindex, which shouldn't be
    modifiable from eBPF.
    
    However, being able to do lookups in devmaps is useful to know (e.g.)
    whether forwarding to a specific interface is enabled. Currently, programs
    work around this by keeping a shadow map of another type which indicates
    whether a map index is valid.
    
    Since we now have a flag to make maps read-only from the eBPF side, we can
    simply lift the lookup restriction if we make sure this flag is always set.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    (cherry picked from commit 0cdbb4b09a0658b72c563638d476113aadd91afb)
    [keyur: keep combined DEVMAP/DEVMAP_HASH case when allowing map_lookup_elem]
    Signed-off-by: Keyur Maru <kd2maru@gmail.com>
    (cherry picked from commit 0d6ceccc8211334a293930cb93006eea7ee163ea)

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3853,13 +3853,10 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 		if (func_id != BPF_FUNC_get_local_storage)
 			goto error;
 		break;
-	/* devmap returns a pointer to a live net_device ifindex that we cannot
-	 * allow to be modified from bpf side. So do not allow lookup elements
-	 * for now.
-	 */
 	case BPF_MAP_TYPE_DEVMAP:
 	case BPF_MAP_TYPE_DEVMAP_HASH:
-		if (func_id != BPF_FUNC_redirect_map)
+		if (func_id != BPF_FUNC_redirect_map &&
+		    func_id != BPF_FUNC_map_lookup_elem)
 			goto error;
 		break;
 	/* Restrict bpf side of cpumap, open when use-cases appear */
