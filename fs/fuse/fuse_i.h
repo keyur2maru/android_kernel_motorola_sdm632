4caf9c9f7b9cb76a7b2d468642d80b952fdbac50
Miklos Szeredi <mszeredi@redhat.com>
2019-09-10 15:04:08 +0200

    fuse: simplify 'nofail' request
    Instead of complex games with a reserved request, just use __GFP_NOFAIL.
    
    Both calers (flush, readdir) guarantee that connection was already
    initialized, so no need to wait for fc->initialized.
    
    Also remove unneeded clearing of FR_BACKGROUND flag.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 40ac7ab2d02176f8a70e37b88e41637ed97b304b)

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -447,9 +447,6 @@ struct fuse_req {
 	/** Request completion callback */
 	void (*end)(struct fuse_conn *, struct fuse_req *);
 
-	/** Request is stolen from fuse_file->reserved_req */
-	struct file *stolen_file;
-
 	/** fuse passthrough file  */
 	struct file *passthrough_filp;
 };
@@ -594,9 +591,6 @@ struct fuse_conn {
 
 	/** waitq for blocked connection */
 	wait_queue_head_t blocked_waitq;
-
-	/** waitq for reserved requests */
-	wait_queue_head_t reserved_req_waitq;
 
 	/** Connection established, cleared on umount, connection
 	    abort and device release */
@@ -956,8 +950,7 @@ void __fuse_get_request(struct fuse_req *req);
 /**
  * Gets a requests for a file operation, always succeeds
  */
-struct fuse_req *fuse_get_req_nofail_nopages(struct fuse_conn *fc,
-					     struct file *file);
+struct fuse_req *fuse_get_req_nofail_nopages(struct fuse_conn *fc);
 
 /**
  * Decrement reference count of a request.  If count goes to zero free
