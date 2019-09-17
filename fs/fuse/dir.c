5b68bdad4c7d98f71e3e7d63d09a1476d5184b8d
Khazhismel Kumykov <khazhy@google.com>
2019-09-17 12:35:33 -0700

    fuse: kmemcg account fs data
    account per-file, dentry, and inode data
    
    blockdev/superblock and temporary per-request data was left alone, as
    this usually isn't accounted
    
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Signed-off-by: Khazhismel Kumykov <khazhy@google.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit dc69e98c241e1456e37d73b862f7b8b8900ba50f)

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -319,7 +319,8 @@ default_path:
 #if BITS_PER_LONG < 64
 static int fuse_dentry_init(struct dentry *dentry)
 {
-	dentry->d_fsdata = kzalloc(sizeof(union fuse_dentry), GFP_KERNEL);
+	dentry->d_fsdata = kzalloc(sizeof(union fuse_dentry),
+				   GFP_KERNEL_ACCOUNT | __GFP_RECLAIMABLE);
 
 	return dentry->d_fsdata ? 0 : -ENOMEM;
 }
--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -73,7 +73,7 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 {
 	struct fuse_file *ff;
 
-	ff = kzalloc(sizeof(struct fuse_file), GFP_KERNEL);
+	ff = kzalloc(sizeof(struct fuse_file), GFP_KERNEL_ACCOUNT);
 	if (unlikely(!ff))
 		return NULL;
 
@@ -83,7 +83,8 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 		ff->passthrough_enabled = 1;
 
 	ff->fc = fc;
-	ff->release_args = kzalloc(sizeof(*ff->release_args), GFP_KERNEL);
+	ff->release_args = kzalloc(sizeof(*ff->release_args),
+				   GFP_KERNEL_ACCOUNT);
 	if (!ff->release_args) {
 		kfree(ff);
 		return NULL;
