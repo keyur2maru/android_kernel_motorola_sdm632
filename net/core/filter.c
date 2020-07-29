12f769df0327de75e70a0b683f0058ebb7324f53
Stanislav Fomichev <sdf@google.com>
2020-07-28 17:31:03 -0700

    bpf: Expose socket storage to BPF_PROG_TYPE_CGROUP_SOCK
    This lets us use socket storage from the following hooks:
    
    * BPF_CGROUP_INET_SOCK_CREATE
    * BPF_CGROUP_INET_SOCK_RELEASE
    * BPF_CGROUP_INET4_POST_BIND
    * BPF_CGROUP_INET6_POST_BIND
    
    Using existing 'bpf_sk_storage_get_proto' doesn't work because
    second argument is ARG_PTR_TO_SOCKET. Even though
    BPF_PROG_TYPE_CGROUP_SOCK hooks operate on 'struct bpf_sock',
    the verifier still considers it as a PTR_TO_CTX.
    That's why I'm adding another 'bpf_sk_storage_get_cg_sock_proto'
    definition strictly for BPF_PROG_TYPE_CGROUP_SOCK which accepts
    ARG_PTR_TO_CTX which is really 'struct sock' for this program type.
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200729003104.1280813-1-sdf@google.com
    (cherry picked from commit f7c6cb1d97285557ba9a4f9b9c8e5b91cb17d970)
    (cherry picked from commit a3bd3a84b02165790f4b8b4e61f55a013783284d)

--- net/core/filter.c
+++ net/core/filter.c
@@ -4383,6 +4383,7 @@ static const struct bpf_func_proto bpf_sock_ops_cb_flags_set_proto = {
 };
 
 const struct bpf_func_proto bpf_event_output_data_proto __weak;
+const struct bpf_func_proto bpf_sk_storage_get_cg_sock_proto __weak;
 
 static const struct bpf_func_proto *
 sock_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
@@ -4397,6 +4398,8 @@ sock_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_get_local_storage_proto;
 	case BPF_FUNC_get_socket_cookie:
 		return &bpf_get_socket_cookie_sock_proto;
+	case BPF_FUNC_sk_storage_get:
+		return &bpf_sk_storage_get_cg_sock_proto;
 	case BPF_FUNC_get_netns_cookie:
 		return &bpf_get_netns_cookie_sock_proto;
 	case BPF_FUNC_perf_event_output:
